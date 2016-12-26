2cdc1e322d563a4a5eb49bc0dcac4d0cbca815df
Ruud van Vliet <ruud@Ruuds-MBP.fritz.box>
2016-12-26 09:13:46 +0100

    Acc aangepast om de score van 11 strikes te berekenen en de overbodige strike + spare weggehaald

--- src/main/scala/vliet.io/BowlingGameDrape.scala
+++ src/main/scala/vliet.io/BowlingGameDrape.scala
@@ -40,9 +40,9 @@ object BowlingGameDrape {
       frames match {
         case Nil => score
         case Strike() +: y +: z +: Nil if frameCount == 9 => score + 10 + y.score + z.score
+        case Strike() +: y +: Nil if frameCount == 9 => score + 10 + y.score
         case Spare(_, _) +: y +: Nil if frameCount == 9 => score + 10 + y.score
         case Strike() +: Strike() +: y +: xs => acc(frames.tail, score + 10 + 10 + y.pins1, frameCount + 1)
-        case Strike() +: Spare(_, _) +: xs => acc(frames.tail, score + 10 + 10, frameCount + 1)
         case Strike() +: x +: xs => acc(frames.tail, score + 10 + x.score, frameCount + 1)
         case Spare(_, _) +: x +: xs => acc(frames.tail, score + 10 + x.pins1, frameCount + 1)
         case x +: xs => acc(frames.tail, score + x.score, frameCount + 1)
