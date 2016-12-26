ec0bfec2a2bbf1adfa4ffd83b81794841f201ed3
Ruud van Vliet <ruud@Ruuds-MBP.fritz.box>
2016-12-26 10:06:10 +0100

    Renamed frames in scoreAcc into framesLeft

--- src/main/scala/vliet.io/BowlingGameFun.scala
+++ src/main/scala/vliet.io/BowlingGameFun.scala
@@ -3,7 +3,7 @@ package vliet.io
 /**
   * Created by ruud on 21/12/2016.
   *
-  * Pure functional implementation:
+  * Pure functional implementation
   */
 object BowlingGameFun {
 
@@ -14,22 +14,22 @@ object BowlingGameFun {
     scoreAcc(rolls, 0, 10)
   }
 
-  private def scoreAcc(rolls: List[Roll], acc: Int, frames: Int): Int = {
-    if (frames == 0) acc
+  private def scoreAcc(rolls: List[Roll], acc: Int, framesLeft: Int): Int = {
+    if (framesLeft == 0) acc
     else rolls match {
       case Nil => acc
       case 10 :: b1 :: b2 :: xs =>
-        require(xs == Nil || frames > 1, "Rolls for a finished game are not allowed")
-        scoreAcc(rolls.tail, acc + 10 + b1 + b2, frames - 1)
+        require(xs == Nil || framesLeft > 1, "Rolls for a finished game are not allowed")
+        scoreAcc(rolls.tail, acc + 10 + b1 + b2, framesLeft - 1)
       case 10 :: b1 :: Nil =>
-        scoreAcc(rolls.tail, acc + 10 + b1, frames - 1)
+        scoreAcc(rolls.tail, acc + 10 + b1, framesLeft - 1)
       case r1 :: r2 :: b1 :: xs if r1 + r2 == 10 =>
-        require(xs == Nil || frames > 1, "Rolls for a finished game are not allowed")
-        scoreAcc(rolls.tail.tail, acc + r1 + r2 + b1, frames - 1)
+        require(xs == Nil || framesLeft > 1, "Rolls for a finished game are not allowed")
+        scoreAcc(rolls.tail.tail, acc + r1 + r2 + b1, framesLeft - 1)
       case r1 :: r2 :: xs =>
         require(r1 + r2 <= 10, "Number of pins within 1 frame must not exceed 10")
-        require(xs == Nil || frames > 1, "Rolls for a finished game are not allowed")
-        scoreAcc(xs, acc + r1 + r2, frames - 1)
+        require(xs == Nil || framesLeft > 1, "Rolls for a finished game are not allowed")
+        scoreAcc(xs, acc + r1 + r2, framesLeft - 1)
       case r1 :: Nil =>
         acc + r1
     }
