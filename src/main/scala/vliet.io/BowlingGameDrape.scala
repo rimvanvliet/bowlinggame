f61e956c13ed32fde9bf131aaf24b04c6f2c38f3
Ruud van Vliet <ruud@Ruuds-MBP.fritz.box>
2016-12-28 11:17:53 +0100

    Reformatted the code, added require text to Frame

--- src/main/scala/vliet.io/BowlingGameDrape.scala
+++ src/main/scala/vliet.io/BowlingGameDrape.scala
@@ -14,21 +14,25 @@ object BowlingGameDrape {
   }
 
   case class Frame(pins1: Int, pins2: Option[Int] = None) {
-    require(pins1 + pins2.getOrElse(0) <= 10)
+    require(pins1 + pins2.getOrElse(0) <= 10, "Number of pins within 1 frame must not exceed 10")
+
     def score: Int = pins1 + pins2.getOrElse(0)
+
     def isStrike: Boolean = pins1 == 10
+
     def isSpare: Boolean = !isStrike && score == 10
   }
 
   def score(rolls: List[Roll]): Int = {
     require(rolls.forall(r => r >= 0 && r <= 10), "Number of pins must be between 0 and 10")
 
-    val frames: Seq[Frame] = rolls.foldLeft((Vector.empty[Frame], Option.empty[Roll])) { case ((framesAcc, unassignedRoll), roll) =>
-      unassignedRoll match {
-        case None if roll == 10 => (framesAcc :+ Frame(roll), None)
-        case None => (framesAcc, Some(roll))
-        case Some(previousRoll) => (framesAcc :+ Frame(previousRoll, Some(roll)), None)
-      }
+    val frames: Seq[Frame] = rolls.foldLeft((Vector.empty[Frame], Option.empty[Roll])) {
+      case ((framesAcc, unassignedRoll), roll) =>
+        unassignedRoll match {
+          case None if roll == 10 => (framesAcc :+ Frame(roll), None)
+          case None => (framesAcc, Some(roll))
+          case Some(previousRoll) => (framesAcc :+ Frame(previousRoll, Some(roll)), None)
+        }
     } match {
       case (framesAcc, None) => framesAcc
       case (framesAcc, Some(rl)) => framesAcc :+ Frame(rl)
@@ -39,7 +43,7 @@ object BowlingGameDrape {
       println(f"frameCount: $frameCount score: ${score} Frames: $frames, ")
       frames match {
         case Nil =>
-          require(frameCount <=10, "Rolls for a finished game are not allowed")
+          require(frameCount <= 10, "Rolls for a finished game are not allowed")
           score
         case Strike() +: y +: z +: Nil if frameCount == 9 => score + 10 + y.score + z.score
         case Strike() +: y +: Nil if frameCount == 9 => score + 10 + y.score
